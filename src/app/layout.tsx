10369be1c4748c5f786fcef32c3160d285b6e28f
Henok-Al <henok.gebresenbet@gmail.com>
2024-08-19 13:09:27 +0300

    fist commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+import Header from "./components/Header/Header";
+
+export const metadata: Metadata = {
+  title: "AddisBlog",
+  description: "Discover latest posts on AddisBlog",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>AddisBLog</div>;
+}
+
+export default App;
