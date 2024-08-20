ac2b718c427fe75bc65bf4d051934afebed8a955
Henok-Al <henok.gebresenbet@gmail.com>
2024-08-20 18:37:45 +0300

    retrieve data from firebase

--- src/app/admin/categories/components/CategoriesListView.tsx
+++ src/app/admin/categories/components/CategoriesListView.tsx
@@ -43,11 +43,9 @@ export default function CategoriesListView() {
                 <td className="border px-4 py-2">{item?.name}</td>
                 <td className="border px-4 py-2">{item?.slug}</td>
                 <td className="border px-4 py-2">
-                  <Link href={`/admin/categories/form?id={item?.id}`}>
-                    <button className="bg-blue-500 text-white rounded-full px-3 py-1 text-sm">
-                      Action
-                    </button>
-                  </Link>
+                  <button className="bg-blue-500 text-white rounded-full px-3 py-1 text-sm">
+                    Action
+                  </button>
                 </td>
               </tr>
             );
