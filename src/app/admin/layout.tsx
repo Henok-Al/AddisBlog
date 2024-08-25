3da0e87216dfe96145ca56142eb9fa03697da60e
Henok-Al <henok.gebresenbet@gmail.com>
2024-08-25 14:47:20 +0300

    RBAC for admin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,19 +1,44 @@
-import AuthContextProvider from "@/lib/contexts/AuthContext";
-import { ReactNode } from "react";
-import Sidebar from "./components/Sidebar";
+"use client";
 
-interface LayoutProps {
+import AuthContextProvider, { useAuth } from "@/lib/contexts/AuthContext";
+import Sidebar from "./components/Sidebar";
+import { useAdmin } from "@/lib/firebase/admin/read";
+import { ReactNode } from "react";
+
+interface AdminLayoutProps {
   children: ReactNode;
 }
-export default function Layout({ children }: LayoutProps) {
+
+export default function Layout({ children }: AdminLayoutProps) {
+  return (
+    <AuthContextProvider>
+      <InnerLayout>{children}</InnerLayout>
+    </AuthContextProvider>
+  );
+}
+
+function InnerLayout({ children }) {
+  const { user, isLoading: authIsLoading } = useAuth();
+  const { data, error, isLoading } = useAdmin({ uid: user?.uid });
+  if (authIsLoading || isLoading) {
+    return <h2>Loading ...</h2>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+  if (!data) {
+    return (
+      <div>
+        <h1>You are not admin</h1>
+      </div>
+    );
+  }
   return (
     <>
-      <AuthContextProvider>
-        <section className="flex">
-          <Sidebar />
-          {children}
-        </section>
-      </AuthContextProvider>
+      <section className="flex">
+        <Sidebar />
+        {children}
+      </section>
     </>
   );
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   <main>
-    <h1>admin panel</h1>
+    <h1 className="text-blue-500    text-2xl ml-72 text-center">admin panel</h1>
   </main>;
 }
